b3bbd8752af0f9d6bbbb576be3db75c8072ed793
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-10-04 22:04:16 -0500

    got the GLTF loader working.  All models can load and appear oriented properly- tested.  Not ussing the board model.

--- sketch.js
+++ sketch.js
@@ -7,134 +7,161 @@ A simple JS starter template for THREE js projects on 1ofX
 */ //////////////////////////////////////////////////////////
 
 let container,
-  scene,
-  camera,
-  renderer,
-  ambLt,
-  dirLt,
-  spotLt,
-  geometry,
-  material,
-  mesh,
-  controls;
+    scene,
+    camera,
+    renderer,
+    ambLt,
+    dirLt,
+    spotLt,
+    geometry,
+    material,
+    mesh,
+    controls;
 let screenShotDone = false;
 
 function init() {
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x000000);
-  setCamera();
-  setLights();
-  buildRenderer();
-  container = renderer.domElement;
-  document.body.appendChild(container);
-  loadAssets();  //  to load a 3d model from ipfs link
-  buildIt();
-  addOrbitControls();
-  window.addEventListener("resize", onWindowResize);
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000000);
+    setCamera();
+    setLights();
+    buildRenderer();
+    container = renderer.domElement;
+    document.body.appendChild(container);
+//  loadAssets();  //  to load a 3d model from ipfs link
+    loadGLTF();
+    buildIt();
+    addOrbitControls();
+    window.addEventListener("resize", onWindowResize);
 }
 
 function animate() {
-  requestAnimationFrame(animate);
-  render();
+    requestAnimationFrame(animate);
+    render();
 }
 
 function render() {
-  updateScene();
-  renderer.render(scene, camera);
+    updateScene();
+    renderer.render(scene, camera);
 }
 
 function updateScene() {
-  if (screenShotDone == false) {
-    // waits for first frame to take screenshot and send features
-    //  add features on next line if desired
-    //  window.OneOfX.save({Hubs: 6, Stages: 3, Gears: 26, Rods: 29, Color_Schemes: 12, Palettes: 130});
-    screenShotDone = true;
-  }
-  // put any scene updates here (rotation of objects for example, etc)
-  controls.update();
+    if (screenShotDone == false) {
+        // waits for first frame to take screenshot and send features
+        //  add features on next line if desired
+        //  window.OneOfX.save({Hubs: 6, Stages: 3, Gears: 26, Rods: 29, Color_Schemes: 12, Palettes: 130});
+        screenShotDone = true;
+    }
+    // put any scene updates here (rotation of objects for example, etc)
+    controls.update();
 }
 
 function setCamera() {
-  camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    1,
-    1000
-  );
-  camera.position.x = 5;
-  camera.position.y = 3;
-  camera.position.z = 5;
-  scene.add(camera);
+    camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    );
+    camera.position.x = 5;
+    camera.position.y = 3;
+    camera.position.z = 5;
+    scene.add(camera);
 }
 
 function setLights() {
-  ambLt = new THREE.AmbientLight(0xffffff, 0.5);
-  scene.add(ambLt);
-  dirLt = new THREE.DirectionalLight(0xffffff, 0.5);
-  dirLt.position.set(0, 15, 0);
-  scene.add(dirLt);
-  spotLt = new THREE.SpotLight(0xffffff, 0.5);
-  spotLt.position.set(5, 1, 2);
-  spotLt.decay = 2.0;
-  scene.add(spotLt);
+    ambLt = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambLt);
+    dirLt = new THREE.DirectionalLight(0xffffff, 0.5);
+    dirLt.position.set(0, 15, 0);
+    scene.add(dirLt);
+    spotLt = new THREE.SpotLight(0xffffff, 0.5);
+    spotLt.position.set(5, 1, 2);
+    spotLt.decay = 2.0;
+    scene.add(spotLt);
 }
 
 function buildRenderer() {
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight, true);
-  renderer.setPixelRatio(window.devicePixelRatio || 1);
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight, true);
+    renderer.setPixelRatio(window.devicePixelRatio || 1);
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function loadAssets() {
-  const loader = new THREE.OBJLoader();
-  let material;
-  let cubeColor = new THREE.Color(Math.random()*255, Math.random()*255, Math.random()*255);
-  // Texture cubes as background
- // loader.load('https://ipfs.io/ipfs/bafybeihsxmq6fqvjzew7sje5fg5ahtr3rytafzrrsdu3bkwuhbnsxvrcmm', function(object) {
-    loader.load('./assets/rook.obj', function(object) {
-    object.name = "chessBoard";
-    object.position = new THREE.Vector3();
-    object.position.x = 0;
-    object.position.y = 0;
-    object.position.z = 0;
-    object.rotation.y = 0;
-    material = new THREE.MeshStandardMaterial({
-      color: cubeColor,
-      metalness: 0.8,
-      roughness: 0.2,
-    });
-object.material = material;
-   scene.add(object);
+    const loader = new THREE.OBJLoader();
+    let material;
+    let cubeColor = new THREE.Color(Math.random() * 255, Math.random() * 255, Math.random() * 255);
+    // Texture cubes as background
+    // loader.load('https://ipfs.io/ipfs/bafybeihsxmq6fqvjzew7sje5fg5ahtr3rytafzrrsdu3bkwuhbnsxvrcmm', function(object) {
+    loader.load('./assets/rook.obj', function (object) {
+        object.name = "chessBoard";
+        object.position = new THREE.Vector3();
+        object.position.x = 0;
+        object.position.y = 0;
+        object.position.z = 0;
+        object.rotation.y = 0;
+        material = new THREE.MeshPhongMaterial({
+            side: THREE.DoubleSide,
+            color: cubeColor,
+            metalness: 0.8,
+            roughness: 0.2,
+        });
+        object.material = material;
+        scene.add(object);
 //    console.log("Spinner - ", object);
-  }, function(xhr) {
+    }, function (xhr) {
 //    console.log((xhr.loaded / xhr.total * 100) + '% of SPinner loaded');
-  }, function(error) {
+    }, function (error) {
 //    console.log('An error happened with Spinner:' + error);
-  });
+    });
+}
+
+function loadGLTF() {
+    const loader = new THREE.GLTFLoader();
+    //   loader.load("./assets/bishop.glb", function (gltf) {
+    //   loader.load("./assets/king.glb", function (gltf) {
+    //   loader.load("./assets/knight.glb", function (gltf) {
+    //   loader.load("./assets/pawn.glb", function (gltf) {
+    //   loader.load("./assets/queen.glb", function (gltf) {
+    loader.load("./assets/rook.glb", function (gltf) {
+        let model = gltf.scene;
+        console.log(model);
+//   gltf.scene.scale.set(0.1, 0.1, 0.1);
+        scene.add(model);
+
+        model.traverse(function (object) {
+            if (object.isMesh) {
+                object.castShadow = true;
+            }
+        });
+    });
 }
 
 function buildIt() {
-  //  put all of your geometry and materials in here
-  geometry = new THREE.BoxGeometry(2, 2, 2);
-  material = new THREE.MeshPhongMaterial({
-    color: "purple",
-    side: THREE.DoubleSide,
-  });
-  mesh = new THREE.Mesh(geometry, material);
+    //  put all of your geometry and materials in here
+
+  // draw the board
+
+
+    geometry = new THREE.BoxGeometry(2, 2, 2);
+    material = new THREE.MeshPhongMaterial({
+        color: "purple",
+        side: THREE.DoubleSide,
+    });
+    mesh = new THREE.Mesh(geometry, material);
 //  scene.add(mesh);
 //  console.log(scene);
 }
 
 function addOrbitControls() {
-  controls = new THREE.OrbitControls(camera, container);
-  controls.minDistance = 5;
-  controls.maxDistance = 500;
+    controls = new THREE.OrbitControls(camera, container);
+    controls.minDistance = 5;
+    controls.maxDistance = 500;
 //  controls.autoRotate = true;
 }
 
